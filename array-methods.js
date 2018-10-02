32a344a286d5eb69974b438c0f8aec8462e9ad83
Ryan Gewondjan <ryan.gewondjan@gmail.com>
2018-10-01 18:05:30 -0600

    started the array-methods.js file and the first method, the push method

---
+++ array-methods.js
@@ -0,0 +1,24 @@
+/*
+
+The "push" method
+
+1. What does it do? make sure to explain all the parameters. If it has a function as a parameter, make sure to explain all of the parameters for that function.
+    The push method appends a value to the end of an array.  It takes any value as a parameter that you would like to append to the end of your array.
+
+2. Does it edit the current array?
+    YES, the current array is editted.
+
+3. What does it return?
+    The push method returns the new length of the array (the length of the array with the appended value)
+
+4.    How can I use this? Come up (not one off the internet) with a small real world example and explain it.
+    I might want to have an array for the number of students in a class and need to add a student who adds the class
+
+5. Build your real world example.
+    see example below: need to change the name.
+*/
+
+function examplePush() {
+
+
+}
